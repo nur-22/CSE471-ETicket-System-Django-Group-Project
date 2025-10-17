ee7a5ffdb7f86742a0558c2d3f840ab9de9c5e4d
Nur-E-Jannat <119960390+nur-22@users.noreply.github.com>
2025-10-18 00:55:51 +0600

    Configure ALLOWED_HOSTS and STATIC_ROOT
    Updated ALLOWED_HOSTS to allow all hosts and set STATIC_ROOT.

--- bracu_ebus/settings.py
+++ bracu_ebus/settings.py
@@ -14,7 +14,7 @@ STRIPE_PUBLISHABLE_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -111,6 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
